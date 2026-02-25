551491d6d94b6b439592d4a1a6414bf0f589d5f7
Firat <studentfirat@gmail.com>
2026-02-25 22:53:20 +0300

    Initial commit

---
+++ Proje/Program.cs
@@ -0,0 +1,25 @@
+﻿string girisMesaji = "Hoşgeldin Kullanıcı, Lütfen geçerli bir kimlik kartı oluşturmak için bilgilerini eksiksiz ve doğru gir.";
+
+Console.WriteLine(girisMesaji);
+
+Console.WriteLine("Ad ve soyadınızı giriniz: ");
+string isim = Console.ReadLine();
+
+Console.WriteLine("Yaşınızı giriniz: ");
+int yas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Memleketinizi giriniz: ");
+string memleket = Console.ReadLine();
+
+Console.Write("Mesleğinizi giriniz: ");
+string meslek = Console.ReadLine();
+
+Console.WriteLine("Kimlik kartınız hazırlanmıştır. Tebrikler :)");
+
+Console.WriteLine("╔══════════════════════════╗");
+Console.WriteLine("       KİMLİK KARTI");
+Console.WriteLine($"  Ad Soyad  : {isim}");
+Console.WriteLine($"  Yaş       : {yas}");
+Console.WriteLine($"  Şehir     : {memleket}");
+Console.WriteLine($"  Meslek    : {meslek}");
+Console.WriteLine("╚══════════════════════════╝");
